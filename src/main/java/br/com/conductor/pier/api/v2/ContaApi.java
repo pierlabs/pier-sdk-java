182458d1039871691ab15dcd4e229e2d74fc8073
Filipe Germano <filipe.germano@conductor.com.br>
2016-10-28 17:16:40 -0300

    SPL002 - Revisando recursos para bliqueio e desbloqueio de cartão.

---
+++ src/main/java/br/com/conductor/pier/api/v2/ContaApi.java
@@ -0,0 +1,164 @@
+package br.com.conductor.pier.api.v2;
+
+import com.sun.jersey.api.client.GenericType;
+
+import br.com.conductor.pier.api.v2.invoker.ApiException;
+import br.com.conductor.pier.api.v2.invoker.ApiClient;
+import br.com.conductor.pier.api.v2.invoker.Configuration;
+import br.com.conductor.pier.api.v2.invoker.Pair;
+
+import br.com.conductor.pier.api.v2.model.Conta;
+import java.util.Date;
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen")
+
+public class ContaApi {
+  private ApiClient apiClient;
+
+  public ContaApi() {
+    this(Configuration.getDefaultApiClient());
+  }
+
+  public ContaApi(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  public ApiClient getApiClient() {
+    return apiClient;
+  }
+
+  public void setApiClient(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  
+  /**
+   * Apresenta dados de uma determinada conta
+   * Este m\u00C3\u00A9todo permite consultar dados de uma determinada conta a partir de seu codigo de identifica\u00C3\u00A7\u00C3\u00A3o (id).
+   * @param idConta C\u00C3\u00B3digo de identifica\u00C3\u00A7\u00C3\u00A3o da conta (id).
+   * @return Conta
+   */
+  public Conta consultarUsingGET1(Long idConta) throws ApiException {
+    Object postBody = null;
+    
+     // verify the required parameter 'idConta' is set
+     if (idConta == null) {
+        throw new ApiException(400, "Missing the required parameter 'idConta' when calling consultarUsingGET1");
+     }
+     
+    // create path and map variables
+    String path = "/api/contas/{id_conta}".replaceAll("\\{format\\}","json")
+      .replaceAll("\\{" + "id_conta" + "\\}", apiClient.escapeString(idConta.toString()));
+
+    // query params
+    List<Pair> queryParams = new ArrayList<Pair>();
+    Map<String, String> headerParams = new HashMap<String, String>();
+    Map<String, Object> formParams = new HashMap<String, Object>();
+
+    
+
+    
+
+    
+
+    final String[] accepts = {
+      "application/json"
+    };
+    final String accept = apiClient.selectHeaderAccept(accepts);
+
+    final String[] contentTypes = {
+      "application/json"
+    };
+    final String contentType = apiClient.selectHeaderContentType(contentTypes);
+
+    String[] authNames = new String[] {"client_id", "access_token" };
+
+    
+    GenericType<Conta> returnType = new GenericType<Conta>() {};
+    return apiClient.invokeAPI(path, "GET", queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
+    
+  }
+  
+  /**
+   * Lista contas existentes na base de dados do Emissor.
+   * Este m\u00C3\u00A9todo permite listar contas existentes na base de dados do Emissor.
+   * @param id C\u00C3\u00B3digo de identifica\u00C3\u00A7\u00C3\u00A3o de conta (id).
+   * @param idProduto C\u00C3\u00B3digo de identifica\u00C3\u00A7\u00C3\u00A3o do produto ao qual a conta faz parte. (id).
+   * @param idOrigemComercial C\u00C3\u00B3digo de Identifica\u00C3\u00A7\u00C3\u00A3o da Origem Comercial (id) que deu origem a Conta.
+   * @param idPessoa C\u00C3\u00B3digo de Identifica\u00C3\u00A7\u00C3\u00A3o da Pessoa Titular da Conta (id).
+   * @param idStatusConta C\u00C3\u00B3digo de Identifica\u00C3\u00A7\u00C3\u00A3o do Produto a qual o cart\u00C3\u00A3o pertence (id).
+   * @param diaVencimento Apresenta o dia de vencimento.
+   * @param melhorDiaCompra Apresenta o melhor dia de compra.
+   * @param dataStatusConta Apresenta a data em que o idStatusConta atual fora atribu\u00C3\u00ADdo para ela.
+   * @param dataCadastro Apresenta a data em que o cart\u00C3\u00A3o foi gerado.
+   * @param page P\u00C3\u00A1gina solicitada (Default = 0)
+   * @param limit Limite de elementos por solicita\u00C3\u00A7\u00C3\u00A3o (Default = 100, Max = 100)
+   * @return Conta
+   */
+  public Conta listarUsingGET1(Long id, Long idProduto, Long idOrigemComercial, Long idPessoa, Long idStatusConta, Integer diaVencimento, Integer melhorDiaCompra, Date dataStatusConta, Date dataCadastro, Integer page, Integer limit) throws ApiException {
+    Object postBody = null;
+    
+    // create path and map variables
+    String path = "/api/contas".replaceAll("\\{format\\}","json");
+
+    // query params
+    List<Pair> queryParams = new ArrayList<Pair>();
+    Map<String, String> headerParams = new HashMap<String, String>();
+    Map<String, Object> formParams = new HashMap<String, Object>();
+
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "id", id));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "idProduto", idProduto));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "idOrigemComercial", idOrigemComercial));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "idPessoa", idPessoa));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "idStatusConta", idStatusConta));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "diaVencimento", diaVencimento));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "melhorDiaCompra", melhorDiaCompra));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "dataStatusConta", dataStatusConta));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "dataCadastro", dataCadastro));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "page", page));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "limit", limit));
+    
+
+    
+
+    
+
+    final String[] accepts = {
+      "application/json"
+    };
+    final String accept = apiClient.selectHeaderAccept(accepts);
+
+    final String[] contentTypes = {
+      "application/json"
+    };
+    final String contentType = apiClient.selectHeaderContentType(contentTypes);
+
+    String[] authNames = new String[] {"client_id", "access_token" };
+
+    
+    GenericType<Conta> returnType = new GenericType<Conta>() {};
+    return apiClient.invokeAPI(path, "GET", queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
+    
+  }
+  
+}
+
