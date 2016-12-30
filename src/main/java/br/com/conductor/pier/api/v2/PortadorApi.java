f092d0e58b8dae51164cffaa85dc240f31b0aa56
Filipe Germano <filipe.germano@conductor.com.br>
2016-12-30 18:13:16 -0300

    Preparando versão para Homologação

--- src/main/java/br/com/conductor/pier/api/v2/PortadorApi.java
+++ src/main/java/br/com/conductor/pier/api/v2/PortadorApi.java
@@ -49,6 +49,7 @@ public class PortadorApi {
    * @param idParentesco C\u00C3\u00B3digo de Identifica\u00C3\u00A7\u00C3\u00A3o do Parentesco (id)
    * @param tipoPortador Apresenta o tipo do Portador do cart\u00C3\u00A3o, sendo: (&#39;T&#39;: Titular, &#39;A&#39;: Adicional).
    * @param nomeImpresso Apresenta o nome a ser impresso no cart\u00C3\u00A3o.
+   * @param idImagem Apresenta o c\u00C3\u00B3digo de identifica\u00C3\u00A7\u00C3\u00A3o da imagem do cart\u00C3\u00A3o.
    * @param idTipoCartao Apresenta o c\u00C3\u00B3digo de identifica\u00C3\u00A7\u00C3\u00A3o do tipo do cart\u00C3\u00A3o (id), que ser\u00C3\u00A1 utilizado para gerar os cart\u00C3\u00B5es deste portador, vinculados a sua respectiva conta atrav\u00C3\u00A9s do campo idConta.
    * @param flagAtivo Quanto ativa, indica que o cadastro do Portador est\u00C3\u00A1 ativo, em emissores que realizam este tipo de gest\u00C3\u00A3o.
    * @param dataCadastroPortador Apresenta a data em que o Portador fora cadastrado, quando possuir esta informa\u00C3\u00A7\u00C3\u00A3o.
@@ -57,7 +58,7 @@ public class PortadorApi {
    * @param limit Limite de elementos por solicita\u00C3\u00A7\u00C3\u00A3o (Default = 100, Max = 100)
    * @return PagePortador
    */
-  public PagePortador listarUsingGET4(Long idConta, Long idProduto, Long idPessoa, Long idParentesco, String tipoPortador, String nomeImpresso, Long idTipoCartao, Integer flagAtivo, Date dataCadastroPortador, Date dataCancelamentoPortador, Integer page, Integer limit) throws ApiException {
+  public PagePortador listarUsingGET4(Long idConta, Long idProduto, Long idPessoa, Long idParentesco, String tipoPortador, String nomeImpresso, Long idImagem, Long idTipoCartao, Integer flagAtivo, Date dataCadastroPortador, Date dataCancelamentoPortador, Integer page, Integer limit) throws ApiException {
     Object postBody = null;
     
     // create path and map variables
@@ -80,6 +81,8 @@ public class PortadorApi {
     queryParams.addAll(apiClient.parameterToPairs("", "tipoPortador", tipoPortador));
     
     queryParams.addAll(apiClient.parameterToPairs("", "nomeImpresso", nomeImpresso));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "idImagem", idImagem));
     
     queryParams.addAll(apiClient.parameterToPairs("", "idTipoCartao", idTipoCartao));
     
