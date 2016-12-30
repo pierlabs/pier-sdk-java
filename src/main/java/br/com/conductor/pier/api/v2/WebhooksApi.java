d8e0369fd7d16f36c7980157bc2d89ce7b4d2afa
Filipe Germano <filipe.germano@conductor.com.br>
2016-12-30 17:47:37 -0300

    Preparando versão para Homologação

---
+++ src/main/java/br/com/conductor/pier/api/v2/WebhooksApi.java
@@ -0,0 +1,285 @@
+package br.com.conductor.pier.api.v2;
+
+import com.sun.jersey.api.client.GenericType;
+
+import br.com.conductor.pier.api.v2.invoker.ApiException;
+import br.com.conductor.pier.api.v2.invoker.ApiClient;
+import br.com.conductor.pier.api.v2.invoker.Configuration;
+import br.com.conductor.pier.api.v2.invoker.Pair;
+
+import br.com.conductor.pier.api.v2.model.WebHook;
+import br.com.conductor.pier.api.v2.model.PageWebHooks;
+
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen")
+
+public class WebhooksApi {
+  private ApiClient apiClient;
+
+  public WebhooksApi() {
+    this(Configuration.getDefaultApiClient());
+  }
+
+  public WebhooksApi(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  public ApiClient getApiClient() {
+    return apiClient;
+  }
+
+  public void setApiClient(ApiClient apiClient) {
+    this.apiClient = apiClient;
+  }
+
+  
+  /**
+   * Alterar Webhook
+   * Este m\u00C3\u00A9todo permite que seja modificado um webhooks j\u00C3\u00A1 cadastrado
+   * @param id C\u00C3\u00B3digo identificador do Webhook
+   * @param evento Evento a ser chamado pelo WebHook
+   * @param metodo M\u00C3\u00A9todo que a ser chamado pelo WebHook
+   * @param url URL que a ser consumida pelo WebHook
+   * @return WebHook
+   */
+  public WebHook alterarUsingPUT2(Long id, Object evento, Object metodo, String url) throws ApiException {
+    Object postBody = null;
+    
+     // verify the required parameter 'id' is set
+     if (id == null) {
+        throw new ApiException(400, "Missing the required parameter 'id' when calling alterarUsingPUT2");
+     }
+     
+     // verify the required parameter 'evento' is set
+     if (evento == null) {
+        throw new ApiException(400, "Missing the required parameter 'evento' when calling alterarUsingPUT2");
+     }
+     
+     // verify the required parameter 'metodo' is set
+     if (metodo == null) {
+        throw new ApiException(400, "Missing the required parameter 'metodo' when calling alterarUsingPUT2");
+     }
+     
+     // verify the required parameter 'url' is set
+     if (url == null) {
+        throw new ApiException(400, "Missing the required parameter 'url' when calling alterarUsingPUT2");
+     }
+     
+    // create path and map variables
+    String path = "/api/webhooks".replaceAll("\\{format\\}","json");
+
+    // query params
+    List<Pair> queryParams = new ArrayList<Pair>();
+    Map<String, String> headerParams = new HashMap<String, String>();
+    Map<String, Object> formParams = new HashMap<String, Object>();
+
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "id", id));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "evento", evento));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "metodo", metodo));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "url", url));
+    
+
+    
+
+    
+
+    final String[] accepts = {
+      "application/json"
+    };
+    final String accept = apiClient.selectHeaderAccept(accepts);
+
+    final String[] contentTypes = {
+      "application/json"
+    };
+    final String contentType = apiClient.selectHeaderContentType(contentTypes);
+
+    String[] authNames = new String[] {"client_id", "access_token" };
+
+    
+    GenericType<WebHook> returnType = new GenericType<WebHook>() {};
+    return apiClient.invokeAPI(path, "PUT", queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
+    
+  }
+  
+  /**
+   * Consultar Webhook
+   * Este m\u00C3\u00A9todo permite que sejam consultado um webhook do emissor atrav\u00C3\u00A9s de um id especifico
+   * @param idWebhook C\u00C3\u00B3digo de Identifica\u00C3\u00A7\u00C3\u00A3o do Webhook (id).
+   * @return WebHook
+   */
+  public WebHook consultarUsingGET9(Long idWebhook) throws ApiException {
+    Object postBody = null;
+    
+     // verify the required parameter 'idWebhook' is set
+     if (idWebhook == null) {
+        throw new ApiException(400, "Missing the required parameter 'idWebhook' when calling consultarUsingGET9");
+     }
+     
+    // create path and map variables
+    String path = "/api/webhooks/{id_webhook}".replaceAll("\\{format\\}","json")
+      .replaceAll("\\{" + "id_webhook" + "\\}", apiClient.escapeString(idWebhook.toString()));
+
+    // query params
+    List<Pair> queryParams = new ArrayList<Pair>();
+    Map<String, String> headerParams = new HashMap<String, String>();
+    Map<String, Object> formParams = new HashMap<String, Object>();
+
+    
+
+    
+
+    
+
+    final String[] accepts = {
+      "application/json"
+    };
+    final String accept = apiClient.selectHeaderAccept(accepts);
+
+    final String[] contentTypes = {
+      "application/json"
+    };
+    final String contentType = apiClient.selectHeaderContentType(contentTypes);
+
+    String[] authNames = new String[] {"client_id", "access_token" };
+
+    
+    GenericType<WebHook> returnType = new GenericType<WebHook>() {};
+    return apiClient.invokeAPI(path, "GET", queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
+    
+  }
+  
+  /**
+   * Lista os Webhooks
+   * Este m\u00C3\u00A9todo permite que sejam listados os webhooks existentes
+   * @param id Id do WebHook
+   * @param evento Evento a ser chamado pelo WebHook
+   * @param metodo M\u00C3\u00A9todo que a ser chamado pelo WebHook
+   * @param url URL que a ser consumida pelo WebHook
+   * @param page P\u00C3\u00A1gina solicitada (Default = 0)
+   * @param limit Limite de elementos por solicita\u00C3\u00A7\u00C3\u00A3o (Default = 100, Max = 100)
+   * @return PageWebHooks
+   */
+  public PageWebHooks listarUsingGET10(Long id, Object evento, Object metodo, String url, Integer page, Integer limit) throws ApiException {
+    Object postBody = null;
+    
+    // create path and map variables
+    String path = "/api/webhooks".replaceAll("\\{format\\}","json");
+
+    // query params
+    List<Pair> queryParams = new ArrayList<Pair>();
+    Map<String, String> headerParams = new HashMap<String, String>();
+    Map<String, Object> formParams = new HashMap<String, Object>();
+
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "id", id));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "evento", evento));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "metodo", metodo));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "url", url));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "page", page));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "limit", limit));
+    
+
+    
+
+    
+
+    final String[] accepts = {
+      "application/json"
+    };
+    final String accept = apiClient.selectHeaderAccept(accepts);
+
+    final String[] contentTypes = {
+      "application/json"
+    };
+    final String contentType = apiClient.selectHeaderContentType(contentTypes);
+
+    String[] authNames = new String[] {"client_id", "access_token" };
+
+    
+    GenericType<PageWebHooks> returnType = new GenericType<PageWebHooks>() {};
+    return apiClient.invokeAPI(path, "GET", queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
+    
+  }
+  
+  /**
+   * Salvar Webhook
+   * Este m\u00C3\u00A9todo permite que seja adicionado um novo webhook
+   * @param evento Evento a ser chamado pelo WebHook
+   * @param metodo M\u00C3\u00A9todo que a ser chamado pelo WebHook
+   * @param url URL que a ser consumida pelo WebHook
+   * @return WebHook
+   */
+  public WebHook salvarUsingPOST3(Object evento, Object metodo, String url) throws ApiException {
+    Object postBody = null;
+    
+     // verify the required parameter 'evento' is set
+     if (evento == null) {
+        throw new ApiException(400, "Missing the required parameter 'evento' when calling salvarUsingPOST3");
+     }
+     
+     // verify the required parameter 'metodo' is set
+     if (metodo == null) {
+        throw new ApiException(400, "Missing the required parameter 'metodo' when calling salvarUsingPOST3");
+     }
+     
+     // verify the required parameter 'url' is set
+     if (url == null) {
+        throw new ApiException(400, "Missing the required parameter 'url' when calling salvarUsingPOST3");
+     }
+     
+    // create path and map variables
+    String path = "/api/webhooks".replaceAll("\\{format\\}","json");
+
+    // query params
+    List<Pair> queryParams = new ArrayList<Pair>();
+    Map<String, String> headerParams = new HashMap<String, String>();
+    Map<String, Object> formParams = new HashMap<String, Object>();
+
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "evento", evento));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "metodo", metodo));
+    
+    queryParams.addAll(apiClient.parameterToPairs("", "url", url));
+    
+
+    
+
+    
+
+    final String[] accepts = {
+      "application/json"
+    };
+    final String accept = apiClient.selectHeaderAccept(accepts);
+
+    final String[] contentTypes = {
+      "application/json"
+    };
+    final String contentType = apiClient.selectHeaderContentType(contentTypes);
+
+    String[] authNames = new String[] {"client_id", "access_token" };
+
+    
+    GenericType<WebHook> returnType = new GenericType<WebHook>() {};
+    return apiClient.invokeAPI(path, "POST", queryParams, postBody, headerParams, formParams, accept, contentType, authNames, returnType);
+    
+  }
+  
+}
+
---
+++ src/main/java/br/com/conductor/pier/api/v2/model/WebHook.java
@@ -0,0 +1,186 @@
+package br.com.conductor.pier.api.v2.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+
+
+
+
+/**
+ * Representa\u00C3\u00A7\u00C3\u00A3o do recurso WebHook
+ **/
+
+@ApiModel(description = "Representa\u00C3\u00A7\u00C3\u00A3o do recurso WebHook")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen")
+public class WebHook   {
+  
+
+
+  public enum EventoEnum {
+    RISCO_FRAUDE("RISCO_FRAUDE"),
+    OUTROS("OUTROS");
+
+    private String value;
+
+    EventoEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return value;
+    }
+  }
+
+  private EventoEnum evento = null;
+  private Long id = null;
+
+
+  public enum MetodoEnum {
+    GET("GET"),
+    POST("POST"),
+    PUT("PUT"),
+    DELETE("DELETE");
+
+    private String value;
+
+    MetodoEnum(String value) {
+      this.value = value;
+    }
+
+    @Override
+    @JsonValue
+    public String toString() {
+      return value;
+    }
+  }
+
+  private MetodoEnum metodo = null;
+  private String url = null;
+
+  
+  /**
+   * Evento a ser chamado pelo WebHook
+   **/
+  public WebHook evento(EventoEnum evento) {
+    this.evento = evento;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", required = true, value = "Evento a ser chamado pelo WebHook")
+  @JsonProperty("evento")
+  public EventoEnum getEvento() {
+    return evento;
+  }
+  public void setEvento(EventoEnum evento) {
+    this.evento = evento;
+  }
+
+  
+  /**
+   * Id do WebHook
+   **/
+  public WebHook id(Long id) {
+    this.id = id;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", required = true, value = "Id do WebHook")
+  @JsonProperty("id")
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  
+  /**
+   * M\u00C3\u00A9todo que a ser chamado pelo WebHook
+   **/
+  public WebHook metodo(MetodoEnum metodo) {
+    this.metodo = metodo;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", required = true, value = "M\u00C3\u00A9todo que a ser chamado pelo WebHook")
+  @JsonProperty("metodo")
+  public MetodoEnum getMetodo() {
+    return metodo;
+  }
+  public void setMetodo(MetodoEnum metodo) {
+    this.metodo = metodo;
+  }
+
+  
+  /**
+   * URL que a ser consumida pelo WebHook
+   **/
+  public WebHook url(String url) {
+    this.url = url;
+    return this;
+  }
+  
+  @ApiModelProperty(example = "null", required = true, value = "URL que a ser consumida pelo WebHook")
+  @JsonProperty("url")
+  public String getUrl() {
+    return url;
+  }
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    WebHook webHook = (WebHook) o;
+    return Objects.equals(this.evento, webHook.evento) &&
+        Objects.equals(this.id, webHook.id) &&
+        Objects.equals(this.metodo, webHook.metodo) &&
+        Objects.equals(this.url, webHook.url);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(evento, id, metodo, url);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class WebHook {\n");
+    
+    sb.append("    evento: ").append(toIndentedString(evento)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    metodo: ").append(toIndentedString(metodo)).append("\n");
+    sb.append("    url: ").append(toIndentedString(url)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
+
+
